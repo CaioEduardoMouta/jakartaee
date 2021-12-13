9c3d4beb96087264626b0c8499fb7004fa297dc1
CAIO MOUTA <caioeduardo1993@gmail.com>
2021-12-12 21:51:02 -0300

    java

--- src/main/java/br/com/alura/job/AgendamentoEmailJob.java
+++ src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -3,23 +3,36 @@ package br.com.alura.job;
 import br.com.alura.entidade.AgendamentoEmail;
 import br.com.alura.servico.AgendamentoEmailServico;
 
-import javax.ejb.Schedule;
-import javax.ejb.Stateless;
+//import javax.ejb.Schedule;
+import javax.annotation.Resource;
+import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 import java.util.List;
 
-@Stateless
+@Singleton
 public class AgendamentoEmailJob {
+
+
 
     @Inject
     private AgendamentoEmailServico agendamentoEmailServico;
 
-    @Schedule(hour = "*", minute = "*", second = "*/10")
+    @Inject
+    @JMSConnectionFactory("java:jboss/DefaultJMSConnectionFactory")
+    private JMSContext context;
+
+    @Resource(mappedName = "java:/jms/queue/EmailQueue")
+    private Queue queue;
+
+    //@Schedule(hour = "*", minute = "*", second = "*/10")
     public void enviarEmail() {
        List<AgendamentoEmail> listarPorNaoAgendado
                = agendamentoEmailServico.listarPorNaoAgendado();
        listarPorNaoAgendado.forEach(emailNaoAgendado -> {
-           agendamentoEmailServico.enviar(emailNaoAgendado);
+           context.createProducer().send(queue,emailNaoAgendado);
            agendamentoEmailServico.alerar(emailNaoAgendado);
        });
     }
